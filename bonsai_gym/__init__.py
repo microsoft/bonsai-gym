c8e1959a6644de849ddfc718c0dc0ed6a4070dc5
Marco Zatta <marco.zatta@microsoft.com>
2022-11-17 15:42:45 +0100

    Add flake8 CI job

--- bonsai_gym/__init__.py
+++ bonsai_gym/__init__.py
@@ -3,6 +3,7 @@ The bonsai-gym-common package contains the classes necessary for using OpenAI
 gym as a simulator for Bonsai BRAIN.
 """
 
-# pyright: reportUnusedImport=false
 
 from .gym_simulator3 import GymSimulator3
+
+__all__ = ("GymSimulator3", )
--- bonsai_gym/serializers.py
+++ bonsai_gym/serializers.py
@@ -1,5 +1,4 @@
 import json
-from typing import Any, Dict, List
 
 import numpy as np
 
